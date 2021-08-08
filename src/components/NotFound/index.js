45b8119b040d5d85a35491a3876da4c00d4b9a0b
Umukoro Chinedu <umukorochinedu@gmail.com>
2021-08-08 10:56:39 -0700

    added login and register route to project

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div className="py-5 mt-5">
-        <div className="py-5 mt-5 notfound-widget">
-          <div className="notfound-content">
+    <div className="px-5 py-5 mt-5">
+        <div className="py-5 mt-5  notfound-widget">
+          <div className=" notfound-content">
               <h1>404</h1>
               <h3>Page Not Found</h3>
               <p>It looks like nothing was found at this location.</p>
