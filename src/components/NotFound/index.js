fad1e3f129122944b41a9d5c80ae105b71d3aa5c
Umukoro Chinedu <umukorochinedu@gmail.com>
2021-08-08 10:48:22 -0700

    added Notfound page to project

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
-    return (
-        <div>
-            <h1>NotFound</h1>
+  return (
+    <div className="py-5 mt-5">
+        <div className="py-5 mt-5 notfound-widget">
+          <div className="notfound-content">
+              <h1>404</h1>
+              <h3>Page Not Found</h3>
+              <p>It looks like nothing was found at this location.</p>
+              <Link to="/" className="btn-notFound">Go Back To Home</Link>
+          </div>
         </div>
-    )
-}
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
