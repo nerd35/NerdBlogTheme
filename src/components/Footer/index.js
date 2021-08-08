a23bb280429af08edf2bda151bf78cc10e6fc365
Umukoro Chinedu <umukorochinedu@gmail.com>
2021-08-08 06:16:08 -0700

    added comment section to single post

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="px-3 py-4 text-center border-top">
+        <div className="container px-3 py-4 text-center border-top">
             <p >© Copyright 2021 <stron className="footer-text">NERD35</stron>, All rights reserved.</p>
         </div>
     )
--- src/components/SinglePostContainer/MainPost.js
+++ src/components/SinglePostContainer/MainPost.js
@@ -4,6 +4,8 @@ import PostImg1 from "../../assets/images/blog/postimg1.jpg";
 import PostImg2 from "../../assets/images/blog/postimg2.jpg";
 import LatestPost from "../../assets/images/blog/latestpost.jpg";
 import Author from "../../assets/images/blog/profilep.JPG";
+import AuthorPic from "../../assets/images/blog/authorpics.jpg";
+
 
 const MainPost = () => {
   return (
@@ -75,43 +77,167 @@ const MainPost = () => {
           </p>
           <br />
           <img src={PostImg2} alt="posting1" className="pt-5" />
-          <br/>
-          <h5  className="post-single-body-title">3 - Pack light , Easy Sustainable Travel Tip</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. sunt in culpa qui officia deserunt mollit anim id e st laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam.</p>
+          <br />
+          <h5 className="post-single-body-title">
+            3 - Pack light , Easy Sustainable Travel Tip
+          </h5>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident. sunt in
+            culpa qui officia deserunt mollit anim id e st laborum.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam.
+          </p>
         </div>
       </div>
-        <div class="row">
-            <div className="col-md-6">
-                <div className="pervious-widget">
-                    <div className="pervious-widget-post">
-                        <div className="small-post">
-                            <div className="small-post-img">
-                                <img src={LatestPost} alt="latestpost" className="latest-post"/>
-                            </div>
-                            <div className="content">
-                                <span className="content-link"><i class="fas fa-long-arrow-alt-left mt-1 me-2"></i> Previous post</span>
-                                <p className="previous-title">7 Healty Dinner Recipes for a Date Night at Home</p>
-                            </div>
-                        </div>
-                    </div>
+      <div class="row">
+        <div className="col-md-6">
+          <div className="pervious-widget">
+            <div className="pervious-widget-post">
+              <div className="small-post">
+                <div className="small-post-img">
+                  <img
+                    src={LatestPost}
+                    alt="latestpost"
+                    className="latest-post"
+                  />
                 </div>
-            </div>
-            <div className="col-md-6">
-                <div className="pervious-widget">
-                    <div className="pervious-widget-post">
-                        <div className="small-post">
-                            <div className="small-post-img">
-                                <img src={PostImg1} alt="latestpost" className="latest-post"/>
-                            </div>
-                            <div className="content">
-                                <span className="content-link"> Next post <i class="fas fa-long-arrow-alt-right ms-2 mt-1"></i></span>
-                                <p className="previous-title">7 Healty Dinner Recipes for a Date Night at Home</p>
-                            </div>
-                        </div>
-                    </div>
+                <div className="content">
+                  <span className="content-link">
+                    <i class="fas fa-long-arrow-alt-left mt-1 me-2"></i>{" "}
+                    Previous post
+                  </span>
+                  <p className="previous-title">
+                    7 Healty Dinner Recipes for a Date Night at Home
+                  </p>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
+        <div className="col-md-6">
+          <div className="pervious-widget">
+            <div className="pervious-widget-post">
+              <div className="small-post">
+                <div className="small-post-img">
+                  <img
+                    src={PostImg1}
+                    alt="latestpost"
+                    className="latest-post"
+                  />
+                </div>
+                <div className="content">
+                  <span className="content-link">
+                    {" "}
+                    Next post{" "}
+                    <i class="fas fa-long-arrow-alt-right ms-2 mt-1"></i>
+                  </span>
+                  <p className="previous-title">
+                    7 Healty Dinner Recipes for a Date Night at Home
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="comment-widget">
+        <div className="mb-3 title">
+          <h5 className="comment-title">3 Comments</h5>
+        </div>
+        <div className="comment-widget-section">
+          <div className="comment-item">
+            <img
+              src={AuthorPic}
+              alt="comentauthor"
+              className="author-coment-img"
+            />
+            <div className="comment-content">
+              <div className="comment-info">
+                <span className="comment-info-text me-3">John Doe</span>
+                <span className="comment-info-text me-3">January 15, 2021</span>
+              </div>
+              <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus at doloremque adipisci eum placeat quod non fugiat aliquid sit similique</span>
+              <div>
+                <p className="comment-link"><i class="fas fa-long-arrow-alt-left me-2"></i> Reply</p>
+              </div>
+            </div>
+          </div>
+          <div className="comment-item">
+            <img
+              src={Author}
+              alt="comentauthor"
+              className="author-coment-img"
+            />
+            <div className="comment-content">
+              <div className="comment-info">
+                <span className="comment-info-text me-3">Chinedu Umukoro</span>
+                <span className="comment-info-text me-3">January 15, 2021</span>
+              </div>
+              <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus at doloremque adipisci eum placeat quod non fugiat aliquid sit similique</span>
+              <div>
+                <p className="comment-link"><i class="fas fa-long-arrow-alt-left me-2"></i> Reply</p>
+              </div>
+            </div>
+          </div>
+          <div className="comment-item">
+            <img
+              src={Author}
+              alt="comentauthor"
+              className="author-coment-img"
+            />
+            <div className="comment-content">
+              <div className="comment-info">
+                <span className="comment-info-text me-3">Chinedu Umukoro</span>
+                <span className="comment-info-text me-3">January 15, 2021</span>
+              </div>
+              <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus at doloremque adipisci eum placeat quod non fugiat aliquid sit similique</span>
+              <div>
+                <p className="comment-link"><i class="fas fa-long-arrow-alt-left me-2"></i> Reply</p>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        <div className="mb-3 title">
+          <h5 className="comment-title">Leave a reply</h5>
+        </div>
+        <form className="form-widget">
+          <p>Your email adress will not be published ,Requied fileds are marked*.</p>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="form-group">
+                <textarea rows="5" cols="30" className="form-control form-control-textarea" placeholder="message*"/>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-group">
+                <input type="text" className="form-control form-control-input" placeholder="Name*"/>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-group">
+                <input type="email" className="form-control form-control-input" placeholder="Email*"/>
+              </div>
+            </div>
+            <div className="mb-20 col-md-12">
+            <div class="form-check">
+            </div>
+              <input class="form-check-input me-3" type="checkbox" value="" id="flexCheckDefault"/>
+              <label class="form-check-label" for="flexCheckDefault">
+              save my name , email and website in this browser for the next time I comment.
+              </label>
+            </div>
+            <div className="mt-4 col-md-12">
+              <button type="submit" className="btn-custom">Post comment</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
